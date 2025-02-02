db121de5cf01dea9bf4f3e295c0c498982bee296
Sourav Kundu <74766939+SouravKunduSK@users.noreply.github.com>
2025-02-02 23:21:03 +0600

    DbContext class added

---
+++ Auth_Data/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth_Data.Models
+{
+    public class AppDbContext:IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            :base(options) 
+        {
+            
+        }
+
+    }
+}
