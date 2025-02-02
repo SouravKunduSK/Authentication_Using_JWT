51bfd486132a3f76cbbb9f2b69c4707461640194
Sourav Kundu <74766939+SouravKunduSK@users.noreply.github.com>
2025-02-03 00:57:07 +0600

    Api created and perform authorization # 1.Register 2.Login 3. Token Generation 4. Logout

---
+++ Auth_Api/Controllers/AuthController.cs
@@ -0,0 +1,107 @@
+﻿using Auth_Data.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Auth_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel registerModel)
+        {
+            if (registerModel == null
+               || string.IsNullOrEmpty(registerModel.FullName)
+               || string.IsNullOrEmpty(registerModel.Email)
+               || string.IsNullOrEmpty(registerModel.Password))
+            {
+                return BadRequest("Invalid registration details");
+            }
+
+            var existingUser = await _userManager.FindByEmailAsync(registerModel.Email);
+            if (existingUser != null)
+            {
+                return Conflict("Email already Exist");
+            }
+
+            var user = new AppUser
+            {
+                FullName = registerModel.FullName,
+                Email = registerModel.Email,
+                UserName = registerModel.Email
+            };
+            var result = await _userManager.CreateAsync(user, registerModel.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            return Ok("Registeration is successful!");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+        {
+            var user = await _userManager.FindByEmailAsync(loginModel.Email);
+            if (user == null)
+            {
+                return Unauthorized(new { success = false, message = "Invalid username or password" });
+            }
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginModel.Password, false);
+            if (!result.Succeeded)
+            {
+                return Unauthorized(new { success = false, message = "Invalid username or password" });
+            }
+
+            var token = GeneratedJwtToken(user);
+            return Ok(new { success = true, token });
+        }
+
+        [HttpPost("Logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Ok("User logged out successfully.");
+        }
+        private string GeneratedJwtToken(AppUser user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub ,user.Id),
+                new Claim(JwtRegisteredClaimNames.Email , user.Email),
+                new Claim("Name" , user.FullName),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                //issuer: _jwtIssuer,
+                //audience: _jwtAudience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(int.Parse(_configuration["Jwt:ExpiryMinutes"])),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ Auth_Data/Models/RegisterModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth_Data.Models
+{
+    public class RegisterModel
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
